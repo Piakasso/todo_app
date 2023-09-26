e53ee9868fef7cc73b90f434fc80fd0d661f10d3
Vitalii <number1pyak@gmail.com>
2023-09-26 17:27:27 +0700

    update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <Router basename="/todo_app">
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
